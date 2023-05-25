348dc2e21383c24adbec6b5197e08a2fc30c5c5d
Ryan Eastman <rking1323@gmail.com>
2023-05-25 15:42:38 -0700

    initial setup is finally done right, nodemon is running

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 require('dotenv').config();
-const { db } = require('./src/api-server/src/models');
-const server = require('./src/api-server/src/server.js');
-const PORT = process.env.PORT || 3001
+const { db } = require('./src/models');
+const server = require('./src/server.js');
+const PORT = process.env.PORT || 3001;
 
 db.sync().then(() => {
   server.start(PORT);
